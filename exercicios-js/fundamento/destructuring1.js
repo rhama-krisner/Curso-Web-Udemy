89bdd5cd8cec7439d51db96b29076ff2a322bc78
Rhama Krisner <rhama.krisner@hotmail.com>
2020-07-03 23:53:05 -0300

    Desestruturação de dados

---
+++ exercicios-js/fundamento/destructuring1.js
@@ -0,0 +1,25 @@
+// Recurso novo ES 2015
+
+const pessoa = {
+    nome: 'Anna',
+    idade: 5,
+    endereco: {
+        logadouro: 'Rua ABC',
+        numero: 1000
+    }
+}
+
+const { nome, idade } = pessoa
+console.log(nome, idade);
+
+const { nome: n, idade: i } = pessoa
+console.log(n, i);
+
+const { sobrenome, bemHumorada = true } = pessoa
+console.log(sobrenome, bemHumorada);
+
+const { endereco: { logadouro, numero, cep } } = pessoa
+console.log(logadouro, numero, cep);
+
+/*const { conta: { ag, num } } = pessoa
+console.log(ag, num);*/
