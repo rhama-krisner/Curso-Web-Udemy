dbec40b254b610deb91f9ea40a628179895c68a0
Rhama Krisner <rhama.krisner@hotmail.com>
2020-07-04 23:31:09 -0300

    Operadores Aritméticos

--- exercicios-js/fundamento/aritmeticos.js
+++ exercicios-js/fundamento/aritmeticos.js
@@ -1,4 +1,15 @@
 const [a, b, c, d] = [3, 5, 1, 15]
 
 const soma = a + b + c + d
+const subtracao = b - d
+const multiplicado = a * b
+const divisao = d / a
+const modulo = a % 2
+console.log(`Soma: ${soma}, Subtração: ${subtracao}, Multiplicação: ${multiplicado}, Divisão: ${divisao}`);
+if (modulo == 0) {
+    console.log(`${modulo} é um valor par`);
+} else{
+    console.log(`${modulo} é um valor ímpar`);
+}
+
 
